2531992da3c2d7e1bb9dece08a7dcf619fe517e3
Joshua Rangsikitpho <chaodyne@thump.local>
2010-02-17 18:15:46 -0800

    Added support for multiple dtos per model.

--- lib/acts_as_dto.rb
+++ lib/acts_as_dto.rb
@@ -10,38 +10,35 @@ module ActsAsDto
     def acts_as_dto(*args)      
       options = args.extract_options!      
       options.assert_valid_keys(VALID_DTO_OPTIONS)      
-      attributes_to_define = args
 
-      module_eval(<<-EVAL, __FILE__, __LINE__)     
-        dto_class_name = #{options.has_key?(:class_name) ? "\"#{options[:class_name]}\"" : "nil"} || self.to_s + "DataTransferObject"
-        dto_method_name = #{options.has_key?(:method_name) ? "\"#{options[:method_name]}\"" : "nil"} || "dto"
-                       
-        Object::const_set(dto_class_name.intern, Class::new do
-          include Dto
-          FIELDS = [#{attributes_to_define.map { |d| ":#{d}"}.join(",")}]
-          attr_accessor *FIELDS
+      field_function_map = args.inject(Hash.new) { |m,arg| arg.is_a?(Array) ? m[arg[0]] = arg[1] : m[arg] = arg; m }
+      
+      dto_class_name = options.has_key?(:class_name) ? options[:class_name] : self.to_s + "DataTransferObject"
+      dto_method_name = options.has_key?(:method_name) ? options[:method_name] : "dto"
+      Object.module_eval(<<-EVAL, __FILE__, __LINE__)     
+        class #{dto_class_name}
+          include ActsAsDto::Dto
+          FIELD_MAP = { #{field_function_map.map{ |field,func| ":#{field} => :#{func}" }.join(",") } }
+          attr_accessor *(FIELD_MAP.keys)
 
           def initialize(obj)
-            FIELDS.each do |attribute|
-              instance_variable_set("@" + attribute.to_s, obj.send(attribute))
+            
+            FIELD_MAP.each do |field,func|
+              instance_variable_set("@" + field.to_s, obj.send(func)) rescue nil
             end
-          end          
-          
-        end)
+          end                    
+        end
         
-        create_dto_method(dto_class_name,dto_method_name)
+      EVAL
+
+      module_eval(<<-EVAL, __FILE__, __LINE__)
+        def #{dto_method_name}
+          #{dto_class_name}.new(self)
+        end      
       EVAL
       
     end
-    
-    private
-    def create_dto_method(class_name,method_name)
-      Rails.logger.info "Creating method #{method_name} for class #{class_name}"
-      module_eval "
-        def #{method_name}
-          #{class_name}.new(self)
-        end"
-    end
+        
   end
   
   module Dto
@@ -49,7 +46,7 @@ module ActsAsDto
       options[:indent] ||= 2
       xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
       xml.instruct! unless options[:skip_instruct]
-      xml.tag!(self.class.name.underscore.dasherize) do
+      xml.tag!(self.class.name.underscore.dasherize.gsub('/', '_')) do
         self.instance_variables.each do |varname|
           var = self.instance_variable_get(varname)
           if var.is_a?(ActsAsDto::Dto)
